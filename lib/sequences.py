16d50512e040f24b35e93f48f2b0c006bedf5bdb
Franklin Hau <fhauchi@gmail.com>
2023-07-09 08:24:33 -0600

    sequence

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        sequence = []
+    elif length == 1:
+        sequence = [0]
+    elif length == 1:
+        sequence = [0]
+    elif length == 2:
+        sequence = [0, 1]
+    else:
+        sequence = [0, 1]
+        while len(sequence) < length:
+            next_num = sequence[-1] + sequence[-2]
+            sequence.append(next_num)
+    print(sequence)
+    
